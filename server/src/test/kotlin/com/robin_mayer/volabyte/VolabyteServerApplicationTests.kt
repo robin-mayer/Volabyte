34f370ab8b684a09ddffe59b9efe3f6de1086c4a
Robin Mayer <robin.mayer@mail.de>
2025-07-16 22:58:23 +0200

    add server application

---
+++ server/src/test/kotlin/com/robin_mayer/volabyte/VolabyteServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.robin_mayer.volabyte
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VolabyteServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
