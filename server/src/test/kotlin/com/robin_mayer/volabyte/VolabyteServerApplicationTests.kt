1819878f89263728893cd285b6b658e96c0d5db8
Robin Mayer <robin.mayer@datagroup.de>
2025-07-18 14:12:45 +0200

    remove failing test that was added for pipeline testing

--- server/src/test/kotlin/com/robin_mayer/volabyte/VolabyteServerApplicationTests.kt
+++ server/src/test/kotlin/com/robin_mayer/volabyte/VolabyteServerApplicationTests.kt
@@ -2,13 +2,11 @@ package com.robin_mayer.volabyte
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import kotlin.test.assertEquals
 
 @SpringBootTest
 class VolabyteServerApplicationTests {
 
 	@Test
 	fun contextLoads() {
-		assertEquals(true, false)
 	}
 }
