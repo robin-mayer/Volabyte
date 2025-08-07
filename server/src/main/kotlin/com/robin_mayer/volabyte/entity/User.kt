f5647374eea706ddf81f5a8660e97605fb3fa811
Robin Mayer <robin.mayer@datagroup.de>
2025-08-07 17:52:43 +0200

    rework user entity t database layer

--- server/src/main/kotlin/com/robin_mayer/volabyte/entity/User.kt
+++ server/src/main/kotlin/com/robin_mayer/volabyte/entity/User.kt
@@ -2,6 +2,7 @@ package com.robin_mayer.volabyte.entity
 
 import com.robin_mayer.volabyte.dto.response.UserDTO
 import com.robin_mayer.volabyte.enums.UserRole
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
@@ -15,6 +16,7 @@ import java.util.UUID
 @Entity
 @Table(name = "users")
 class User (
+    @Column(unique = true)
     var userName: String,
     var displayName: String,
     var password: String,
@@ -24,8 +26,11 @@ class User (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Suppress("unused")
     var id: Long? = null
+    @Column(unique = true)
     val userId = UUID.randomUUID().toString()
+    @Suppress("unused")
     val createdAt = Date()
     var lastLoginAt = Date()
 
