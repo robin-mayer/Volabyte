2bea20cee36c9434a6d9223a6a6f3a33035712fe
Robin Mayer <robin.mayer@mail.de>
2025-08-15 10:23:48 +0200

    add sidebar

--- web-app/src/pages/DashboardPage.tsx
+++ web-app/src/pages/DashboardPage.tsx
@@ -1,12 +1,35 @@
-import type React from "react";
+import React from "react";
 import Header from "../components/Header";
 import type { AuthUser } from "../models/AuthUser";
+import Sidebar from "../components/Sidebar";
+import { Box } from "@mui/material";
 
 const DashboardPage: React.FC<{ authUser: AuthUser; setAuthUser: any }> = ({
   authUser,
   setAuthUser,
 }) => {
-  return <Header authUser={authUser} setAuthUser={setAuthUser} />;
+  const headerHeight = 4;
+
+  return (
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Sidebar height={headerHeight} />
+      <Box sx={{ width: "100%" }}>
+        <Header
+          height={headerHeight}
+          authUser={authUser}
+          setAuthUser={setAuthUser}
+        />
+        <Box
+          sx={{
+            width: "100%",
+            height: `calc(100% - ${headerHeight}rem)`,
+            border: "1px solid rgba(0, 0, 0, 0.12)",
+            borderTopLeftRadius: "10px",
+          }}
+        ></Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default DashboardPage;
