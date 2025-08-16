4d7d700bea81a2b314a0645d5bdbba87be0ca66f
Robin Mayer <62951451+robin-mayer@users.noreply.github.com>
2025-08-16 13:35:08 +0200

    feature/#24-implement-sidebar-in-dashboard-page

---
+++ web-app/src/components/Sidebar.tsx
@@ -0,0 +1,83 @@
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Drawer,
+  Box,
+  Typography,
+  Link,
+} from "@mui/material";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+
+const drawerWidth = 200;
+
+const Sidebar: React.FC<{ height: number }> = ({ height }) => {
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          border: "none",
+          display: "flex",
+          flexDirection: "column",
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Box
+        sx={{
+          height: `${height}rem`,
+          display: "flex",
+          alignItems: "center",
+          pl: "1rem",
+        }}
+      >
+        <img
+          src="public/images/logo_128.png"
+          alt="Volabyte logo"
+          width="140px"
+        />
+      </Box>
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+          pt: "4rem",
+          pb: "1rem",
+        }}
+      >
+        <List sx={{ width: "100%" }}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Inventory2Icon />
+              </ListItemIcon>
+              <ListItemText primary={"Files"} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Link
+          href="https://github.com/robin-mayer/Volabyte"
+          target="_blank"
+          rel="noreferrer"
+          color="textPrimary"
+        >
+          <Typography variant="body2" gutterBottom>
+            GitHub
+          </Typography>
+        </Link>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
--- web-app/src/pages/DashboardPage.tsx
+++ web-app/src/pages/DashboardPage.tsx
@@ -1,12 +1,35 @@
-import type React from "react";
+import React from "react";
 import Header from "../components/Header";
 import type { AuthUser } from "../models/AuthUser";
+import Sidebar from "../components/Sidebar";
+import { Box } from "@mui/material";
 
 const DashboardPage: React.FC<{ authUser: AuthUser; setAuthUser: any }> = ({
   authUser,
   setAuthUser,
 }) => {
-  return <Header authUser={authUser} setAuthUser={setAuthUser} />;
+  const headerHeightInRem = 4;
+
+  return (
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Sidebar height={headerHeightInRem} />
+      <Box sx={{ width: "100%" }}>
+        <Header
+          height={headerHeightInRem}
+          authUser={authUser}
+          setAuthUser={setAuthUser}
+        />
+        <Box
+          sx={{
+            width: "100%",
+            height: `calc(100% - ${headerHeightInRem}rem)`,
+            border: "1px solid rgba(0, 0, 0, 0.12)",
+            borderTopLeftRadius: "8px",
+          }}
+        ></Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default DashboardPage;
