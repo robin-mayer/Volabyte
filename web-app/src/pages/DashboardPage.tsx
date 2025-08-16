b2e91648d39bb83813e4c41efa423adef1763121
Robin Mayer <robin.mayer@datagroup.de>
2025-08-16 13:33:50 +0200

    code cleanup

--- web-app/src/pages/DashboardPage.tsx
+++ web-app/src/pages/DashboardPage.tsx
@@ -8,23 +8,23 @@ const DashboardPage: React.FC<{ authUser: AuthUser; setAuthUser: any }> = ({
   authUser,
   setAuthUser,
 }) => {
-  const headerHeight = 4;
+  const headerHeightInRem = 4;
 
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
-      <Sidebar height={headerHeight} />
+      <Sidebar height={headerHeightInRem} />
       <Box sx={{ width: "100%" }}>
         <Header
-          height={headerHeight}
+          height={headerHeightInRem}
           authUser={authUser}
           setAuthUser={setAuthUser}
         />
         <Box
           sx={{
             width: "100%",
-            height: `calc(100% - ${headerHeight}rem)`,
+            height: `calc(100% - ${headerHeightInRem}rem)`,
             border: "1px solid rgba(0, 0, 0, 0.12)",
-            borderTopLeftRadius: "10px",
+            borderTopLeftRadius: "8px",
           }}
         ></Box>
       </Box>
