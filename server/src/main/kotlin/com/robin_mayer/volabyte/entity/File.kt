81a2326c316a85f74334ff0e9f03f9430a69dad6
Robin Mayer <robin.mayer@datagroup.de>
2025-08-17 19:13:47 +0200

    add unique constraint to referenced file

--- server/src/main/kotlin/com/robin_mayer/volabyte/entity/File.kt
+++ server/src/main/kotlin/com/robin_mayer/volabyte/entity/File.kt
@@ -1,5 +1,6 @@
 package com.robin_mayer.volabyte.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -12,6 +13,7 @@ import java.util.Date
 class File (
     var name: String,
     var isDirectory: Boolean,
+    @Column(unique = true)
     var referencedFile: String?,
     var parentDirectoryId: Long?,
     var ownerId: String
