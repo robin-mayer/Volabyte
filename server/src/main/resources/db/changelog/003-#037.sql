0e73b4c710bd37f01249f40be1601bf7c6bc6ecd
Robin Mayer <robin.mayer@datagroup.de>
2025-08-30 04:03:31 +0200

    make id new primary key in user table and drop unique constraint

--- server/src/main/resources/db/changelog/003-#037.sql
+++ server/src/main/resources/db/changelog/003-#037.sql
@@ -4,3 +4,5 @@
 ALTER TABLE users DROP COLUMN id;
 DROP SEQUENCE IF EXISTS users_id_seq;
 ALTER TABLE users RENAME COLUMN user_id TO id;
+ALTER TABLE users ADD PRIMARY KEY (id);
+ALTER TABLE users DROP CONSTRAINT users_user_id_unique;
