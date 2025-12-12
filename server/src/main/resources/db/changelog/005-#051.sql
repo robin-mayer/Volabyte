187f0fad5358adb1acd92042fda5ac70438ebf3b
Robin Mayer <robin.mayer@datagroup.de>
2025-12-12 16:52:40 +0100

    add new constraint for the combination of userId and deviceId

--- server/src/main/resources/db/changelog/005-#051.sql
+++ server/src/main/resources/db/changelog/005-#051.sql
@@ -2,3 +2,8 @@
 
 -- changeset Robin.Mayer:1765554428-1
 ALTER TABLE sessions DROP CONSTRAINT sessions_device_id_unique;
+
+-- changeset Robin.Mayer:1765554428-2
+ALTER TABLE sessions
+    ADD CONSTRAINT sessions_user_id_device_id_unique
+    UNIQUE (user_id, device_id);
