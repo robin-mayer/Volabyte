3f4fce53c6381fdec14ca9f6a0533d1694d9b9a5
Robin Mayer <robin.mayer@datagroup.de>
2025-12-12 11:41:13 +0100

    add settings container

---
+++ web-app/src/container/SettingsContainer.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+
+const SettingsContainer = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        gap: "1rem",
+      }}
+    >
+      Settings Page
+    </Box>
+  );
+};
+
+export default SettingsContainer;
