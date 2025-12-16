2075c9178389bfdfb080862c4cc562c11784eb1d
Robin Mayer <robin.mayer@datagroup.de>
2025-12-16 13:38:09 +0100

    add initial folder creation

--- server/src/main/kotlin/com/robin_mayer/volabyte/component/StartupRunner.kt
+++ server/src/main/kotlin/com/robin_mayer/volabyte/component/StartupRunner.kt
@@ -1,6 +1,7 @@
 package com.robin_mayer.volabyte.component
 
 import com.robin_mayer.volabyte.service.UserService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
@@ -9,8 +10,21 @@ class StartupRunner (
     private val userService: UserService
 ): CommandLineRunner {
 
+    @Value("\${storage.path}")
+    private lateinit var storagePath: String
+    @Value("\${storage.temp-folder}")
+    private lateinit var tempFolder: String
+    @Value("\${storage.files-folder}")
+    private lateinit var filesFolder: String
+
     override fun run(vararg args: String?) {
+        generateInitialFolderStructure()
         generateInitialAdmin()
+    }
+
+    private fun generateInitialFolderStructure() {
+        java.io.File(storagePath, tempFolder).mkdirs()
+        java.io.File(storagePath, filesFolder).mkdirs()
     }
 
     private fun generateInitialAdmin() {
